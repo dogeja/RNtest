e520f9744861a13b687a20e1ba613bdd39f312c9
dogeja <disk6473@gmail.com>
2023-12-02 13:32:01 +0900

    feat:btnTheme

--- App.js
+++ App.js
@@ -11,8 +11,11 @@ export default function App() {
       <View style={styles.imageContainer}>
         <ImageViewer placeHolderImgSource={placeHolderImg} />
       </View>
+      <View style={styles.footerContanier}>
+        <Button theme="primary" label={"CHOOSE A PHOTO"} />
+        <Button label={"USE THIS PHOTO"} />
+      </View>
       <StatusBar style="auto" />
-      <Button label={"버튼"} />
     </View>
   );
 }
@@ -27,5 +30,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContanier: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
--- components/Button.js
+++ components/Button.js
@@ -1,26 +1,67 @@
 import { View, Pressable, StyleSheet, Text } from "react-native";
-export default function Button({ label }) {
+import { FontAwesome } from "@expo/vector-icons";
+export default function Button({ theme, label }) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.BtnWrap,
+          { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.pressableBtn, { backgroundColor: "#fff" }]}
+          onPress={() => {
+            console.log(`YOU"VE CLICKED BUTTON`);
+          }}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
   return (
-    <View>
+    <View style={styles.BtnWrap}>
       <Pressable
         style={styles.pressableBtn}
         onPress={() => {
-          console.log("you've benn pressed.");
+          console.log(`YOU"VE CLICKED BUTTON`);
         }}
       >
-        <Text>{label}</Text>
+        <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
   );
 }
+// styles
 const styles = StyleSheet.create({
+  BtnWrap: {
+    marginHorizontal: 20,
+    width: 320,
+    height: 68,
+    padding: 3,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   pressableBtn: {
-    backgroundColor: "f2f2f2",
-    borderRadius: 8,
+    backgroundColor: "#efa2f2",
     width: "100%",
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
+    borderRadius: 18,
+  },
+  buttonLabel: {
+    color: "#fff",
+    fontSize: 15,
   },
 });
